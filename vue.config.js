284095de0ca8dda7701b54a187bf7573207ff64d
Antério Vieira <anteriovieira@gmail.com>
2020-05-30 15:30:43 -0300

    chore: configure colors

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pwa: {
+    name: 'Crew demo',
+    shortName: 'Crew demo',
+    display: 'standalone',
+    themeColor: '#1a1a1a',
+    msTileColor: '#1a1a1a',
+    background_color: '#1a1a1a',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    startUrl: '/?standalone=true',
+    manifestOptions: {
+      'background_color': '#1a1a1a'
+    }
+  }
 }
